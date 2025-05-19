34566dd942655b63bb8d4c8c48c0c2173380c5da
edk128129 <kostonedu@gmail.com>
2025-05-19 08:22:24 -0300

    Add files via upload

---
+++ ex2/java.js
@@ -0,0 +1,26 @@
+function maior(){
+
+    let num1 = 0;
+    const ano = 2025
+    
+ num1 = Number(window.prompt("em que ano voce nasceu: "));
+ 
+ let idade = ano - num1
+ 
+ alert(idade)
+ 
+ if(idade > 18){
+     alert("voce pode tirar a carteira de motorista");
+ }
+ if(idade == 18){
+     alert("voce pode tirar a carteira de motorista");
+ }
+ else{
+     let quant = 18 - idade
+ alert("voce tem q esperar " + quant , "anos")
+ alert
+ }
+ 
+ 
+ 
+ }
---
+++ ex3/java.js
@@ -0,0 +1,32 @@
+function somar(){
+     num1 = Number(window.prompt("primeiro numero:  "));
+     num2 = Number(window.prompt("segundo numero:"));
+    
+     num3 = num1 + num2 
+
+     alert("o resultado da soma e "+num3 )
+}
+function diminuir(){
+     num1 = Number(window.prompt("numero 1: "));
+     num2 = Number(window.prompt("numero 2: "));
+    
+     num3 = num1 - num2 
+
+     alert("o resultado da subtração e "+num3 );
+}
+function divisao(){
+     num1 = Number(window.prompt("numero 1: "));
+     num2 = Number(window.prompt("numero 2: "));
+    
+     num3 = num1 / num2 
+
+     alert("o resultado da diviçao e "+num3 )
+}
+function multiplicaçao(){
+     num1 = Number(window.prompt("numero 1: "));
+     num2 = Number(window.prompt("numero 2: "));
+    
+     num3 = num1 * num2 
+
+     alert("o resultado da multiplicaçao e "+num3 )
+}
